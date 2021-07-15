b8a3875a78a5dd8d7012e416cf4340e258b84a57
Bruno Matias <30832455+rnbruno@users.noreply.github.com>
2021-07-15 17:01:55 -0300

    Update requisitos.php

--- Ponto/Cadastrar_afastamento/requisitos.php
+++ Ponto/Cadastrar_afastamento/requisitos.php
@@ -11,4 +11,27 @@ log_cadastro_afastamento
 apontar_usuario
   bloqueado;
 
+//Criando ROTINA MySQL
+CREATE EVENT IF NOT EXISTS _nome_
+
+ON SCHEDULE EVERY 6 HOUR
+
+STARTS CURRENT_TIMESTAMP
+
+DO
+
+INSERT INTO `evento_afastamento`(`user_domain`, `user_afastado`, `data_inicio`, `data_final`, `tipo_afastamento`, `starting_ending`) VALUES (12,23,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,4,0)
+
+
+/*
+CREATE EVENT IF NOT EXISTS event_3
+ON SCHEDULE EVERY 2 SECOND
+
+START CURRENT_TIMESTAMP
+
+ENDS CURRENT_TIMESTAMP + INTERVAL 20 SECOND
+
+DO
+INSERT INTO `evento_afastamento`(`user_domain`, `user_afastado`, `data_inicio`, `data_final`, `tipo_afastamento`, `starting_ending`) VALUES (12,23,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,4,0)
+*/
 ?>
